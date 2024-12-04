09219db50e18ec38f6dfd4f3ee40302224bcabfb
Austin Maggert <maggertaustin@gmail.com>
2024-12-03 19:34:43 -0800

    Initial create book page

---
+++ src/views/books/create.tsx
@@ -0,0 +1,72 @@
+'use client';
+
+import * as React from 'react';
+import CssBaseline from '@mui/material/CssBaseline';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { Alert } from '@mui/material';
+
+// Project imports
+import CreateBookForm from 'sections/books/book-forms/createBook';
+
+const defaultTheme = createTheme();
+
+interface IAlert {
+  showAlert: boolean;
+  alertMessage: string;
+  alertSeverity: string;
+}
+
+const EMPTY_ALERT: IAlert = {
+  showAlert: false,
+  alertMessage: '',
+  alertSeverity: ''
+};
+
+export default function CreateBook() {
+  const [alert, setAlert] = React.useState(EMPTY_ALERT);
+
+  const onSuccess = () => {
+    setAlert({
+      showAlert: true,
+      alertMessage: 'The book was added!',
+      alertSeverity: 'success'
+    });
+  };
+
+  const onError = (message: string) => {
+    setAlert({
+      showAlert: true,
+      alertMessage: 'Book not added! Error: ' + message,
+      alertSeverity: 'error'
+    });
+  };
+
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      {alert.showAlert && (
+        <Alert severity={alert.alertSeverity as any} onClose={() => setAlert(EMPTY_ALERT)}>
+          {alert.alertMessage}
+        </Alert>
+      )}
+      <Container component="main" maxWidth="sm">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center'
+          }}
+        >
+          <Typography component="h1" variant="h5">
+            Add a Book
+          </Typography>
+          <CreateBookForm onSuccess={onSuccess} onError={onError} />
+        </Box>
+      </Container>
+    </ThemeProvider>
+  );
+}
