e5cd411e7f4a03d2d1e7475ce351aa8971ea9a8c
AlvVal282 <alvarovd@outlook.com>
2024-12-04 16:31:06 -0800

    Deleted uncessary folders and files before merging with main branch

--- src/menu-items/messages.tsx
+++ src/menu-items/messages.tsx
@@ -24,28 +24,6 @@ const pages: NavItemType = {
   type: 'group',
   children: [
     {
-      id: 'messages',
-      title: <FormattedMessage id="messages" />,
-      type: 'collapse',
-      icon: icons.MessageOutlined,
-      children: [
-        {
-          id: 'send-message',
-          title: <FormattedMessage id="send-message" />,
-          type: 'item',
-          url: '/messages/send',
-          icon: icons.SendIcon
-        },
-        {
-          id: 'view-messages',
-          title: <FormattedMessage id="view-messages" />,
-          type: 'item',
-          url: '/messages/list',
-          icon: icons.EmailIcon
-        }
-      ]
-    },
-    {
       id: 'books',
       title: <FormattedMessage id="books" defaultMessage="Books" />,
       type: 'collapse',
